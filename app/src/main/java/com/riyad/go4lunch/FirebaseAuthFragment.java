4f4a7af448c296a41d239af3821c2fe2808d0116
Riyad <riyad.henna@yahoo.fr>
2020-03-11 08:36:12 +0100

    add Twitter sdk for use twitter login

--- app/src/main/java/com/riyad/go4lunch/FirebaseAuthFragment.java
+++ app/src/main/java/com/riyad/go4lunch/FirebaseAuthFragment.java
@@ -9,7 +9,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class FirebaseAuthFragment extends Fragment {
+
+    private static final int RC_SIGN_IN = 123;
+
+    public static FirebaseAuthFragment newInstace(){
+        return new FirebaseAuthFragment();
+    }
 
     public FirebaseAuthFragment(){
 
@@ -19,10 +30,37 @@ public class FirebaseAuthFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
         return inflater.inflate(R.layout.firebase_auth_fragment_blank, container, false);
+
+    }
+
+
+    public void creatSignInIntent(){
+
+        // Google, Facebook and twitter authentification providers
+        List<AuthUI.IdpConfig> proviaders = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build()
+//                new AuthUI.IdpConfig.TwitterBuilder().build()
+        );
+
+        //Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setTheme(R.style.LoginTheme)
+                .setAvailableProviders(proviaders)
+                .setIsSmartLockEnabled(false,true)
+                .setLogo(R.drawable.icon1)
+                .build(),RC_SIGN_IN
+        );
 
 
     }
+
+
+
 
 
 }
--- app/src/main/java/com/riyad/go4lunch/MainActivity.java
+++ app/src/main/java/com/riyad/go4lunch/MainActivity.java
@@ -1,14 +1,64 @@
 package com.riyad.go4lunch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.coordinatorlayout.widget.CoordinatorLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+    private CoordinatorLayout coordinatorLayout;
+    private Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        coordinatorLayout = findViewById(R.id.coordinator);
+        buttonLogin = findViewById(R.id.main_activity_button_login);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createSignInIntent();
+            }
+        });
+
+
     }
+
+    public void createSignInIntent() {
+        // [START auth_fui_create_intent]
+        // Choose authentication providers
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.GoogleBuilder().build(),
+                new AuthUI.IdpConfig.FacebookBuilder().build());
+
+
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setTheme(R.style.LoginTheme)
+                        .setAvailableProviders(
+                                providers)
+                        .setIsSmartLockEnabled(false, true)
+                        .setLogo(R.drawable.icon1)
+                        .build(), RC_SIGN_IN
+        );
+
+    }
+
+
 }
