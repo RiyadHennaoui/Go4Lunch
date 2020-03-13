9ccfa7c8deb7f6c07518209a7c19aae753fbd9e2
Riyad <riyad.henna@yahoo.fr>
2020-03-13 15:17:03 +0100

    add ProfileActivity

---
+++ app/src/main/java/com/riyad/go4lunch/ProfileActivity.java
@@ -0,0 +1,55 @@
+package com.riyad.go4lunch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class ProfileActivity extends AppCompatActivity {
+    private TextView textInputEditTextUsername;
+    private TextView textViewEmail;
+    private ImageView imageViewProfile;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        textInputEditTextUsername = findViewById(R.id.profile_activity_edit_text_username);
+        textViewEmail= findViewById(R.id.profile_activity_text_view_email);
+        imageViewProfile = findViewById(R.id.profile_activity_imageview_profile);
+
+        this.isCurrentUserLogged();
+        this.updateUIWhenCreating();
+    }
+
+    private FirebaseUser getCurrentUser(){ return FirebaseAuth.getInstance().getCurrentUser(); }
+
+    private Boolean isCurrentUserLogged(){ return (this.getCurrentUser() !=null); }
+
+    private void updateUIWhenCreating(){
+
+        if (this.getCurrentUser().getPhotoUrl() !=null){
+            Glide.with(this)
+                    .load(this.getCurrentUser().getPhotoUrl())
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(imageViewProfile);
+        }
+
+        String email = TextUtils.isEmpty(this.getCurrentUser().getEmail()) ?
+               getString(R.string.info_no_email_found)  : this.getCurrentUser().getEmail();
+        String username = TextUtils.isEmpty(this.getCurrentUser().getDisplayName()) ?
+                getString(R.string.info_no_username_found) : this.getCurrentUser().getDisplayName();
+
+        this.textInputEditTextUsername.setText(username);
+        this.textViewEmail.setText(email);
+
+    }
+}
