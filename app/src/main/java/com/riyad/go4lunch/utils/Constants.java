8e08606f749e65277e8b8c9c22d875a1216d80fa
Riyad <riyad.henna@yahoo.fr>
2020-03-24 11:58:35 +0100

    add Constants class

---
+++ app/src/main/java/com/riyad/go4lunch/utils/Constants.java
@@ -0,0 +1,9 @@
+package com.riyad.go4lunch.utils;
+
+public class Constants {
+
+    public static final int ERROR_DIALOG_REQUEST = 9001;
+    public static final int PERMISSION_REQUEST_ENABLE_GPS = 9002;
+    public static final int PERMISSION_REQUEST_ACCESS_FINE_LOCATION = 9003;
+
+}
