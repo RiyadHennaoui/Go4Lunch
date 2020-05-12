fef761014440de6d69dc984e8f72e9a1c4f5bf83
riyad <riyad.hennaoui@gmail.com>
2020-05-12 22:04:35 +0200

    detail activity added

---
+++ app/src/main/java/com/riyad/go4lunch/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.riyad.go4lunch;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+
+    }
+}
