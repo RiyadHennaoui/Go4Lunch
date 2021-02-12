3ac0a45269e9153dc37f657ed972a497acff4fb3
riyad <riyad.hennaoui@gmail.com>
2021-02-12 18:43:16 +0100

    remove Logs and clean code

--- app/src/main/java/com/riyad/go4lunch/worker/WorkerDelateRestaurant.java
+++ app/src/main/java/com/riyad/go4lunch/worker/WorkerDelateRestaurant.java
@@ -1,11 +1,8 @@
 package com.riyad.go4lunch.worker;
 
 import android.content.Context;
-import android.util.Log;
-
 import androidx.annotation.NonNull;
 import androidx.work.OneTimeWorkRequest;
-import androidx.work.PeriodicWorkRequest;
 import androidx.work.WorkManager;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
@@ -13,7 +10,6 @@ import androidx.work.WorkerParameters;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.riyad.go4lunch.model.User;
 import com.riyad.go4lunch.ui.Restaurant;
 
 import org.jetbrains.annotations.NotNull;
@@ -28,11 +24,7 @@ import static com.riyad.go4lunch.utils.Constants.COLLECTION_RESTAURANTS_NAME;
 public class WorkerDelateRestaurant extends Worker {
 
     FirebaseFirestore firebaseFirestore = FirebaseFirestore.getInstance();
-    private CollectionReference userDb;
     private CollectionReference restaurantDb;
-    private ArrayList<String> workmatesBookId = new ArrayList<>();
-    private User user;
-    private Restaurant restaurant;
 
 
     public WorkerDelateRestaurant(@NonNull Context context, @NonNull WorkerParameters workerParams) {
@@ -42,14 +34,6 @@ public class WorkerDelateRestaurant extends Worker {
     @NonNull
     @Override
     public Result doWork() {
-        Log.e("DoWorkDelate", "inDo work");
-        //TODO revoir la modélisation suite au problème de BookingRestaurant et BoonkingUser;
-        //TODO appel firestore pour récupérer l'utilisateur
-        //TODO si le booking restaurant n'est pas null, prendre l'id du restaurant et récuperer ce restaurant dans firestore.
-        //TODO récuperer la liste des users qui ont reservé sans oublier d'enlever le current user.
-        //TODO appeler show notification.
-
-
 
         restaurantDb = firebaseFirestore.collection(COLLECTION_RESTAURANTS_NAME);
 
@@ -71,58 +55,31 @@ public class WorkerDelateRestaurant extends Worker {
 
                             for (QueryDocumentSnapshot queryDocumentSnapshot : task.getResult()){
                                 Restaurant restaurant = queryDocumentSnapshot.toObject(Restaurant.class);
-                                Log.e("restauget", restaurant.getName());
                                 restaurantsList.add(restaurant);
                                 restaurantDb
                                         .document(restaurant.getId())
-                                        .delete()
-                                        .addOnCompleteListener(task1 -> {
-                                            Log.e("dans la suppresion3", "delate");
-                                            if(task1.isCanceled()){
-                                                Log.e("dans la suppression3", "erreur");
-                                            }
-                                        });
+                                        .delete();
                             }
                         }
                     }
                 });
 
         for (int i= 0;i < restaurantsList.size() ; i++ ){
-            Log.e("dans le supp2", restaurantsList.get(i).getName());
             restaurantDb
                     .document(restaurantsList.get(i).getId())
-                    .delete()
-                    .addOnCompleteListener(task -> {
-                        Log.e("dans la suppresion2", "delate");
-                        if(task.isCanceled()){
-                            Log.e("dans la suppression2", "erreur");
-                        }
-                    });
+                    .delete();
         }
-
 
         return restaurantsList;
 
     }
 
     private void suppressAllRestaurant(List<Restaurant> getRestaurant){
-        Log.e("dans le supp", getRestaurant.size() + " !");
         for (int i= 0;i < getRestaurant.size() ; i++ ){
-            Log.e("dans le supp", getRestaurant.get(i).getName());
             restaurantDb
                     .document(getRestaurant.get(i).getId())
-                    .delete()
-                    .addOnCompleteListener(task -> {
-                        Log.e("dans la suppresion", "delate");
-                        if(task.isCanceled()){
-                            Log.e("dans la suppression", "erreur");
-                        }
-                    })
-            .addOnFailureListener(e -> Log.e("Failure", e.toString()));
-
+                    .delete();
         }
-
-
     }
 
     public static void deleteRestaurantsPeriodRequest(Context context) {
@@ -131,7 +88,6 @@ public class WorkerDelateRestaurant extends Worker {
 
         OneTimeWorkRequest periodicWorkRequest = new OneTimeWorkRequest.Builder(WorkerDelateRestaurant.class)
                 .setInitialDelay(delay, TimeUnit.MILLISECONDS)
-                .addTag("delete, worker")
                 .build();
 
 
@@ -149,12 +105,10 @@ public class WorkerDelateRestaurant extends Worker {
 
 
         Long delay = noonDate.getTimeInMillis() - actualDate.getTimeInMillis();
-        Log.e("delayDelete", (delay / 60000) + "");
         if (delay < 0){
             Calendar tomorrowDate = Calendar.getInstance();
             tomorrowDate.add(Calendar.HOUR_OF_DAY, 24);
             delay = tomorrowDate.getTimeInMillis() - actualDate.getTimeInMillis() - Math.abs(delay);
-            Log.e("delayIfDelete", (delay / 60000) + "");
         }
         return delay;
     }
